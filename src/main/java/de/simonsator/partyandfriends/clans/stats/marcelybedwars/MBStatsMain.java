9210c789d2132d13e719ade59dfe39fd69dd48bf
Simonsator <00pflaume@gmail.com>
2025-10-18 14:16:49 +0200

    Add spigot version support

--- src/main/java/de/simonsator/partyandfriends/clans/stats/marcelybedwars/MBStatsMain.java
+++ src/main/java/de/simonsator/partyandfriends/clans/stats/marcelybedwars/MBStatsMain.java
@@ -4,8 +4,8 @@ import de.simonsator.partyandfriends.api.PAFExtension;
 import de.simonsator.partyandfriends.clan.commands.ClanCommands;
 import de.simonsator.partyandfriends.clan.commands.subcommands.Stats;
 import de.simonsator.partyandfriends.communication.sql.MySQLData;
+import de.simonsator.partyandfriends.utilities.ConfigurationCreator;
 import de.simonsator.partyandfriends.utilities.Language;
-import net.md_5.bungee.config.Configuration;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,19 +16,18 @@ import java.util.regex.Pattern;
  * @version 1.0.0 17.01.17
  */
 public class MBStatsMain extends PAFExtension {
-	private Configuration config;
-	private MBStatsConnection connection;
-	private Configuration messagesConfig;
+	private ConfigurationCreator config;
+	private ConfigurationCreator messagesConfig;
 
 	public void onEnable() {
 		try {
-			config = (new MBStatsConfig(new File(getConfigFolder(), "config.yml"), this)).getCreatedConfiguration();
-			messagesConfig = (new MBStatsMessages(Language.OWN, new File(getDataFolder(), "messages.yml"), this)).getCreatedConfiguration();
+			config = (new MBStatsConfig(new File(getConfigFolder(), "config.yml"), this));
+			messagesConfig = (new MBStatsMessages(Language.OWN, new File(getDataFolder(), "messages.yml"), this));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		MySQLData mySQLData = new MySQLData(config.getString("database.host"), config.getString("database.user"), config.getString("database.password"), config.getInt("database.port"), config.getString("database.db"), "", config.getBoolean("database.use-ssl"));
-		connection = new MBStatsConnection(mySQLData);
+		MBStatsConnection connection = new MBStatsConnection(mySQLData);
 		((Stats) ClanCommands.getInstance().getSubCommand(Stats.class)).registerClanStats(new MBStats(messagesConfig.getString("Name"), connection, Pattern.compile("[KILLS]", Pattern.LITERAL).matcher(messagesConfig.getString("Kills")), Pattern.compile("[WINS]", Pattern.LITERAL).matcher(messagesConfig.getString("Wins")), Pattern.compile("[DEFEATS]", Pattern.LITERAL).matcher(messagesConfig.getString("Defeats")), Pattern.compile("[DEATHS]", Pattern.LITERAL).matcher(messagesConfig.getString("Deaths")), Pattern.compile("[DESTROYED]", Pattern.LITERAL).matcher(messagesConfig.getString("DestroyedBeds")), Pattern.compile("[PLAYED]", Pattern.LITERAL).matcher(messagesConfig.getString("Played")), Pattern.compile("[RANK]", Pattern.LITERAL).matcher(messagesConfig.getString("Rank"))), this);
 		registerAsExtension();
 	}
